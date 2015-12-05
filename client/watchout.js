afbdd53c9b7aa7cd239c5235c937f2fd95913890
joksina <jksalaudeen@yahoo.com>
2015-12-05 10:20:26 -0800

    some changes

--- client/watchout.js
+++ client/watchout.js
@@ -1,113 +1,123 @@
-// start slingin' some d3 here.
+var svg = d3.select('body').append('svg');
 
-// var gameOptions = {
-//   height: 450,
-//   width: 700,
-//   nEnemies: 30,
-//   enemyRadius: 20,
-//   padding: 20
-// };
+var enemies = 20;
+var array = [];
+var score = 0;
+var highScore = 0;
+var collisions = 0;
+var collisionTimer;
+var collided = false;
 
-// var gameStats = {
-//   score: 0,
-//   bestScore: 0
-// };
+for (var i = 0; i < enemies; i++) {
+  array[i] = i;
+  //console.log(i);
+}
 
-// var axes = {
-//   x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
-//   y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
-// };
+var positions = function() {
+  return _.map(array, function(i) {
+    return {
+      id: i,
+      x: Math.random() * 600,
+      y: Math.random() * 500,
+      rotation: 0
+    };
+  });
+};
 
-// var gameBoard = d3.select('.container').append('svg:svg')
-//                 .attr('width', gameOptions.width)
-//                 .attr('height', gameOptions.height);
+var enemy = positions();
 
-// var updateScore = function(){
-//   d3.select(".current span")
-//     .text(gameStats.score.toString())
-//     // check about toString
-// };
+var asteroids = svg.selectAll('image')
+  .data(enemy)
+  .enter()
+  .append('image')
+  .attr('xlink:href', 'asteroid.png')
+  .attr('x', function(d) { return d.x; })
+  .attr('y', function(d) { return d.y; })
+  .attr('class', 'asteroid')
+  .attr('height', 20)
+  .attr('width', 20);
 
-// var updateBestScore = function() {
-//   gameStats.bestScore = _.max([gameStats.bestScore, gameStats.score]);
-// };
-var w =500;
-var h =500;
+var move = function () {
+  asteroids.transition()
+    .attr('x', function(d) {
+      d.x = Math.random() * 600;
+      return d.x;
+    })
+    .attr('y', function(d) {
+      d.y = Math.random() * 500;
+      return d.y;
+    })
+    .duration(1000);
+};
 
-var x = 250;
-var y = 250;
-//Add an id for D3
-var canvas = d3.select('body')
-  .append('svg')
-  .attr('width', w)
-  .attr('height', h);
+//set the data for x and y axis
+var player = svg.append('circle')
+  .data([{x: 300, y:250}])
+  .attr('class', 'player')
+  .attr('fill', 'blue')
+  .attr('r', 10)
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) { return d.y; });
 
-var pointMaker = function(n) {
-  // body..
-  var array = [];
-    for (var i = 0; i < n; i++) {
-      var newX = Math.random() * 100000 % w;
-      var newY = Math.random() * 100000 % h;
-      array.push({x:newX, y:newY});
+var collisionCheck = function(x, y) {
+  var threshold = 20;
+  var p = player.data()[0];
+  var distance = Math.sqrt ( Math.pow((p.x - x), 2) + Math.pow((p.y - y), 2) );
+  if (distance <= threshold) {
+    collided = true;
+    setTimeout(function() {
+      collided = false;
+    }, 900);
+
+    score = 0;
+    collisions++;
+
+    d3.select('.collisions span').text(collisions);
   }
-  return array;
 };
 
 
-
 var drag = d3.behavior.drag()
-            // .on("drag", dragStart)
-            .on('drag', function(){
-              player.attr("x", d3.event.x)
-                    .attr("y", d3.event.y)
+  .on('drag', function(){
+              player.attr("cx", d3.event.x)
+                    .attr("cy", d3.event.y);
             });
 
+var updateCurrentScore = function() {
+  score++;
+  if (score > highScore) {
+    highScore = score;
+  }
+  d3.select('.current span').text(score);
+  d3.select('.highscore span').text(highScore);
 
+};
+scoreTimer = setInterval(updateCurrentScore, 5);
 
-var dataArray = pointMaker(5);
-for (var i =0; i<dataArray.length; i++){
-  var circle = canvas.append('circle')
-
-                .attr('cx', dataArray[i].x)
-                .attr('cy', dataArray[i].y)
-                .attr('r', 30)
-                .attr('fill', 'red')
-}
-setInterval(function () {
-var arr = pointMaker(5);
-d3.select('svg').selectAll('circle').data(arr)
-      .transition()
-      .duration(1000)
-      .attr({
-        cx: function (d) { return d.x},
-        cy: function (d) { return d.y},
-      });
-    }, 1000);
-
-
-function dragStart(d){
-  d3.event.sourceEvent.stopPropagation();
+var collisionStart = function() {
+  if (!collided) {
+    asteroids.each(function() {
+      var singleAsteroid = d3.select(this)
+      collisionCheck(singleAsteroid.attr('x'), singleAsteroid.attr('y'));
+    })
+  }
 }
 
+var spinShuriken = function() {
+  asteroids.each(function() {
+    d3.select(this).attr('transform', function(d) {
+      d.rotation = d.rotation + 2;
+      return 'rotate(' +  d.rotation + ' '
+        + (d3.select(this).attr('x') - 10) + ' '
+        + (d3.select(this).attr('y') - 10) + ')'
+    })
+  })
+}
 
-var player = canvas.append('rect')
-                .attr('x', w/2)
-                .attr('y', h/2)
-                .attr('height', 30)
-                .attr('width', 30)
-                // .attr('r', 15)
-                .attr('fill', 'blue')
-                .call(drag);
+// setInterval(spinShuriken, 1);
 
+collisionTimer = setInterval(collisionStart, 5);
 
+player.call(drag);
 
-//player.attr("x", d.x = Math.max(0, Math.min(w - player.width, d3.event.x)))
-
-
-
-
-
-
-
-
-
+setInterval(move, 1000);
